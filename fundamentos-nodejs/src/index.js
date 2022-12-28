20a76c51ef246bbf085b9f3746fb0c97553b8d60
Gabriel <gabriel.gsm2017@gmail.com>
2022-12-28 13:34:53 -0300

    feat: Criando rotas api

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -2,10 +2,26 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello world!" });
+app.get("/courses", (request, response) => {
+  return response.json([["curso 1", "curso 2", "curso 3"]]);
 });
 
-app.listen(3001, () => {
+app.post("/courses", (request, response) => {
+  return response.json([["curso 1", "curso 2", "curso 3", "curso 4"]]);
+});
+
+app.put("/courses/:id", (request, response) => {
+  return response.json([["curso 6", "curso 2", "curso 3"]]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json([["curso 6", "curso 7", "curso 3"]]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json([["curso 6", "curso 7"]]);
+});
+
+app.listen(8080, () => {
   console.log("--- servidor rodando ---");
 });
