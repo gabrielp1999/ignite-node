ae540bc6c67a70f6494c8a44ed3904dc1008f280
Gabriel Pereira <gabrielpereira@MacBook-Air-de-Gabriel.local>
2023-01-21 22:00:58 -0300

    feat:configurando eslint

--- 3-typescript/server.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { CreateCourse } from "./routes";
-
-const app = express();
-
-app.get("/", CreateCourse);
-
-app.listen(3001, () => console.log("---- servidor rodando na 3001 ----"))
